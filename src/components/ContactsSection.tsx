ef3f3000021dea6d2628a9e2380706e1f805d462
Dr. Max Kushnaryov <mauxion@gmail.com>
2026-02-27 08:29:48 +0200

    contacts upd

--- src/components/ContactsSection.tsx
+++ src/components/ContactsSection.tsx
@@ -11,17 +11,17 @@ export function ContactsSection() {
           <div className="mt-3 space-y-2 text-sm text-gray-200">
             <div className="flex justify-between gap-2">
               <span className="text-gray-400">Телефон</span>
-              <a className="hover:underline" href="tel:+380000000000">+38 (098) 02-666-98</a>
+              <a className="hover:underline" href="tel:+380000000000">+38 (063) 683-37-58</a>
             </div>
             <div className="flex justify-between gap-2">
               <span className="text-gray-400">Telegram</span>
-              <a className="hover:underline" href="https://t.me/Maxon_87" target="_blank" rel="noreferrer">
-                @Maxon_87
+              <a className="hover:underline" href="https://t.me/@backline_com_ua" target="_blank" rel="noreferrer">
+                @backline_com_ua
               </a>
             </div>
             <div className="flex justify-between gap-2">
               <span className="text-gray-400">Email</span>
-              <a className="hover:underline" href="mailto:mauxion@gmail.com">mauxion@gmail.com</a>
+              <a className="hover:underline" href="mailto:mail@backline.com.ua">mail@backline.com.ua</a>
             </div>
             <div className="flex justify-between gap-2">
               <span className="text-gray-400">Місто</span>
